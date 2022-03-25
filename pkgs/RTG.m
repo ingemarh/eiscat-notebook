03b17ec607f031c11a6b7bdb4336d6208bbe7a25
Ingemar Häggström <ingemar@eiscat.se>
2022-03-25 13:47:47 +0100

    with display

--- pkgs/RTG.m
+++ pkgs/RTG.m
@@ -1,2 +1,1 @@
 addpath('/opt/remtg/lib')
-rtg_startup
--- pkgs/guisdap.m
+++ pkgs/guisdap.m
@@ -1,7 +1,8 @@
 homedir=getenv('HOME');
-if ~exist(fullpath(homedir,'gup'),'dir'), mkdir(fullpath(homedir,'gup')), end
-if ~exist(fullpath(homedir,'gup','mygup'),'dir'), mkdir(fullpath(homedir,'gup','mygup')), end
-if ~exist(fullpath(homedir,'gup','results'),'dir'), mkdir(fullpath(homedir,'gup','results')), end
-if ~exist(fullpath(homedir,'tmp'),'dir'), mkdir(fullpath(homedir,'tmp')), end
+if ~exist(fullfile(homedir,'gup'),'dir'), mkdir(fullfile(homedir,'gup')), end
+if ~exist(fullfile(homedir,'gup','mygup'),'dir'), mkdir(fullfile(homedir,'gup','mygup')), end
+if ~exist(fullfile(homedir,'gup','results'),'dir'), mkdir(fullfile(homedir,'gup','results')), end
+if ~exist(fullfile(homedir,'tmp'),'dir'), mkdir(fullfile(homedir,'tmp')), end
 addpath([homedir,'/gup/mygup'],'/opt/guisdap/anal','/opt/guisdap/init')
+clear homedir
 startup
